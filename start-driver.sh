ddd754ee2afe56a77142b5b7fbeb0b77f1d7c4f0
joseLuís <joseluis@andamira.net>
2018-03-10 12:09:17 +0100

    improve script, make it more solid

--- start-driver.sh
+++ start-driver.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
-# Adjust this as needed
+
+# Adjust these as needed
 DRIVER="kamvas.py"
+UCLOGIC_BINS="/usr/local/bin/"
+
 
 T="256c:006e"
 BUS=$(lsusb | grep "$T" | sed -e 's|Bus \([0-9]*\) Device \([0-9]*\):.*$|\1|g')
 DEV=$(lsusb | grep "$T" | sed -e 's|Bus \([0-9]*\) Device \([0-9]*\):.*$|\2|g')
 
-sudo rmmod hid_uclogic
-sudo modprobe uinput
-sudo /usr/local/bin/uclogic-probe $BUS $DEV | /usr/local/bin/uclogic-decode
-sudo python3 $DRIVER
+
+MODULE_OLD=hid_uclogic
+MODULE_NEW=uinput
+
+if lsmod | grep "$MODULE_OLD" &> /dev/null ; then
+	sudo rmmod "$MODULE_OLD"
+fi
+
+if lsmod | grep "$MODULE_NEW" &> /dev/null ; then
+	sudo rmmod "$MODULE_NEW"
+fi
+sudo modprobe "$MODULE_NEW"
+
+
+sudo "$UCLOGIC_BINS/uclogic-probe" "$BUS" "$DEV" | "$UCLOGIC_BINS/uclogic-decode"
+
+sudo python3 "$DRIVER"
